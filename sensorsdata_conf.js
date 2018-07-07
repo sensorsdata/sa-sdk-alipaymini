0b27905a9dcf513c148343657b71dcdbcbad9c96
shengyonggen <spiritpanda@sina.com>
2018-07-07 14:58:47 +0800

    0.1版本

---
+++ sensorsdata_conf.js
@@ -0,0 +1,8 @@
+var conf ={
+  // 神策分析数据接收地址
+  server_url: 'https://test-syg.datasink.sensorsdata.cn/sa.gif?token=27f1e21b78daf376',
+  // 传入的字符串最大长度限制
+  max_string_length: 300
+};
+
+export default conf;
