86505c6f970cb61190f49071179f52ce67fcfc2c
付登荣 <dengrongf@jumei.com>
2018-08-30 17:23:48 +0800

    系统及版本取值错误

--- sensorsdata.js
+++ sensorsdata.js
@@ -456,8 +456,8 @@ _.info = {
             e.$model = t["model"];
             e.$screen_width = Number(t["windowWidth"]);
             e.$screen_height = Number(t["windowHeight"]);
-            e.$os = t["system"].split(' ')[0];
-            e.$os_version = t["system"].split(' ')[1];
+            e.$os = t["platform"];
+            e.$os_version = t["version"];
             that.setStatusComplete();
           },
           complete : that.setStatusComplete
