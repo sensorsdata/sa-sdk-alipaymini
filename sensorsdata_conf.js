0bd7808d5566869e4dd8d3a83aa98e864355dee0
shengyonggen <shengyonggen@sensorsdata.cn>
2021-09-16 11:50:55 +0800

    Release 1.1.6

--- sensorsdata_conf.js
+++ sensorsdata_conf.js
@@ -2,9 +2,9 @@ var conf = {
   // 神策分析数据接收地址
   server_url: '',
   // 传入的字符串最大长度限制
-  max_string_length: 300,
+  max_string_length: 500,
   datasend_timeout: 25000,
-  show_log: true // 默认打印日志
+  show_log: false // 默认关闭 log 日志
 };
 
 export default conf;
